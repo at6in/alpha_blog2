806f42dc576bef1344c4562ded853b142a612b4e
Atbin Jahandideh <atbinjahandideh@gmail.com>
2017-11-08 21:21:46 +0000

    add edit update show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,14 @@
 class ArticlesController < ApplicationController
     
+    def index
+        @articles = Article.all
+    end
     def new
         @article = Article.new
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
     end
     
     def create
@@ -18,6 +25,15 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
     
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was succesfully created"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
     
     private
     
